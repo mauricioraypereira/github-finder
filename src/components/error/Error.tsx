992a5bc48e811b7fee2cdc2892a586db9e70c851
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-25 20:11:34 -0300

    Add error component

---
+++ src/components/error/Error.tsx
@@ -0,0 +1,9 @@
+const Error = () => {
+  return (
+    <div>
+        <p>Usuário(a) não foi encontrado(a)!</p>
+    </div>
+  )
+};
+
+export default Error;
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,15 +2,24 @@ import { UserProps } from "../types/user";
 
 import Search from "../components/search/Search";
 import User from "../components/user/User";
+import Error from "../components/error/Error";
 
 import { useState } from "react";
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState(false);
 
     const loadUser = async(userName: string) => {
+        setError(false);
+        setUser(null);
         const response = await fetch(`https://api.github.com/users/${userName}`);
         const data = await response.json();
+
+        if (response.status === 404) {
+            setError(true);
+            return;
+        }
 
         const {avatar_url, login, location, followers, following} = data;
 
@@ -29,6 +38,7 @@ const Home = () => {
         <div>
             <Search loadUser={loadUser} />
             {user && <User {...user} />}
+            {error && <Error />}
         </div>
     )
 };
