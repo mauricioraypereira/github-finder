83f69e3c79dfd44991b1654f8b36b3e46c93caed
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-25 20:51:49 -0300

    Getting repositories from githubapi by username

--- src/routes/repositories/Repositories.tsx
+++ src/routes/repositories/Repositories.tsx
@@ -1,17 +1,43 @@
+import { RepositoriesProps } from '../../types/repositories';
+
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import BackButton from '../../components/buttons/backbutton/BackButton';
+import Loader from '../../components/loader/Loader';
 
 import styles from './Repositories.module.css';
 
 const Repositories = () => {
-  const  { username } = useParams();
+    const  { username } = useParams();
 
-  return <div>
-    <BackButton />
-    Repositórios de {username}
-  </div>
+    const [repositories, setRepositories] = useState<RepositoriesProps[] | [] | null>(null);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        const loadResponse = async function(username: string) {
+            setIsLoading(true);
+            
+            const response = await fetch(`https://api.github.com/users/${username}/repos`);
+            
+            const dataResponse = await response.json();
+
+            setIsLoading(false);
+        };
+
+        if (username) {
+            loadResponse(username);
+        }
+
+
+        
+    }, [])
+
+    return <div>
+        <BackButton />
+        Repositórios de {username}
+    </div>
 }
 
 export default Repositories
