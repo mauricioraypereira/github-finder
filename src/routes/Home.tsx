856e0811e3cafa86afb33fa5882dc099709c8fb5
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-25 19:10:06 -0300

    Adding types + getting user from githubapi

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,22 @@
+import { UserProps } from "../types/user";
+
 import Search from "../components/Search";
 
+import { useState } from "react";
+
 const Home = () => {
-  return (
-    <Search />
-  )
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    const loadUser = async(userName: string) => {
+        const response = await fetch(`https://api.github.com/users/${userName}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    return (
+        <Search loadUser={loadUser}/>
+    )
 };
 
 export default Home;
