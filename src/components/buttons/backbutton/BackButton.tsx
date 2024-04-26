b4d498ebe8ea65aff96b1e3ac135df8d97bf2243
Mauricio Ray <mauricioraypereira@gmail.com>
2024-04-26 08:47:48 -0300

    Styles to repositories page

--- src/components/buttons/backbutton/BackButton.tsx
+++ src/components/buttons/backbutton/BackButton.tsx
@@ -1,11 +1,17 @@
 import { useNavigate } from "react-router-dom"
 
+import styles from './BackButton.module.css';
+
 const BackButton = () => {
     const navigate = useNavigate();
 
     return (
         <>
-            <button onClick={() => navigate(-1)}>Voltar</button>
+            <button 
+                className={styles.back_button}
+                onClick={() => navigate(-1)}>
+                    Voltar
+            </button>
         </>
     )
 }
--- src/routes/repositories/Repositories.tsx
+++ src/routes/repositories/Repositories.tsx
@@ -34,18 +34,23 @@ const Repositories = () => {
         }
     }, [])
 
-    return <div>
-        <BackButton />
-        <h2>Explore os repositórios do usuário: {username}</h2>
-        {repositories && repositories.length === 0 && <p>Não há repositórios.</p>}
-        {repositories && repositories.length > 0 && (
-            <div>
-                {repositories.map((repository: RepositoriesProps) => (
-                    <Repository key={repository.name} {...repository} />
-                ))}
-            </div>
-        )}
-    </div>
+    if (!repositories && isLoading) return <Loader />;
+
+    return (
+        <div className={styles.repositories_main}>
+            <BackButton />
+            <h2>Explore os repositórios do usuário: {username}</h2>
+            {repositories && repositories.length === 0 && <p>Não há repositórios.</p>}
+            {repositories && repositories.length > 0 && (
+                <div className={styles.repositories_container}>
+                    {repositories.map((repository: RepositoriesProps) => (
+                        <Repository key={repository.name} {...repository} />
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+        
 }
 
 export default Repositories
